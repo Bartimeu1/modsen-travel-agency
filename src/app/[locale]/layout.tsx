f062c677a356742d35452d8521456fdc83546628
ISapego <iv.sap11@gmail.com>
2024-08-29 13:29:41 +0300

    feat(*): add base routes layout

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+
+import { Header } from '@components/Header';
+
+import { getFontsClasses } from '@utils/get-fonts-classes';
+
+import { baseFonts } from '@root/constants';
+import { ApolloWrapper } from '@root/lib/ApolloWrapper';
+
+import '@styles/index.scss';
+
+export const metadata = {
+  title: 'Modsen Travel Agency',
+  description: 'Best travel agency right here',
+};
+
+interface RootLayoutProps {
+  children: ReactNode;
+  params: { locale: string };
+}
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: RootLayoutProps) {
+  const messages = useMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={getFontsClasses(baseFonts)}>
+        <ApolloWrapper>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <Header />
+            {children}
+          </NextIntlClientProvider>
+        </ApolloWrapper>
+      </body>
+    </html>
+  );
+}
